fa6058a34c8d618d388a358b41eb0f7054c99c16
KarvaMies <jeremias.moberg@gmail.com>
2023-06-08 12:51:43 +0300

    possible fix to task 2

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -62,42 +62,47 @@ document.getElementById('search-input').addEventListener('keydown', (event) => {
 });
 
 function updateRecipeView(recipe) {
-  let recipeContainer = document.getElementById("container");
-  recipeContainer.innerHTML = '';
+  document.addEventListener("DOMContentLoaded", function() {
+    // Your JavaScript code here
+    let recipeContainer = document.getElementById("container");
+    recipeContainer.innerHTML = '';
 
-  if (recipe) {
-    var recipeName = document.createElement("h2");
-    recipeName.innerText = recipe.name;
+    if (recipe) {
+      var recipeName = document.createElement("h2");
+      recipeName.innerText = recipe.name;
 
-    var instructionsHeading = document.createElement("h3");
-    instructionsHeading.innerText = "Instructions:";
+      var instructionsHeading = document.createElement("h3");
+      instructionsHeading.innerText = "Instructions:";
 
-    var instructionsList = document.createElement("ul");
-    recipe.instructions.forEach(function(instruction) {
-      var li = document.createElement("li");
-      li.innerText = instruction;
-      instructionsList.appendChild(li);
-    });
+      var instructionsList = document.createElement("ul");
+      recipe.instructions.forEach(function(instruction) {
+        var li = document.createElement("li");
+        li.innerText = instruction;
+        instructionsList.appendChild(li);
+      });
 
-    var ingredientsHeading = document.createElement("h3");
-    ingredientsHeading.innerText = "Ingredients:";
+      var ingredientsHeading = document.createElement("h3");
+      ingredientsHeading.innerText = "Ingredients:";
 
-    var ingredientsList = document.createElement("ul");
-    recipe.ingredients.forEach(function(ingredient) {
-      var li = document.createElement("li");
-      li.innerText = ingredient;
-      ingredientsList.appendChild(li);
-    });
+      var ingredientsList = document.createElement("ul");
+      recipe.ingredients.forEach(function(ingredient) {
+        var li = document.createElement("li");
+        li.innerText = ingredient;
+        ingredientsList.appendChild(li);
+      });
 
     
-    recipeContainer.appendChild(recipeName);
-    recipeContainer.appendChild(instructionsHeading);
-    recipeContainer.appendChild(instructionsList);
-    recipeContainer.appendChild(ingredientsHeading);
-    recipeContainer.appendChild(ingredientsList);
-  } else {
-    let notFound = document.createElement("p");
-    notFound.innerText = "Recipe not found";
-    recipeContainer.append(notFound);
-  }
+      recipeContainer.appendChild(recipeName);
+      recipeContainer.appendChild(instructionsHeading);
+      recipeContainer.appendChild(instructionsList);
+      recipeContainer.appendChild(ingredientsHeading);
+      recipeContainer.appendChild(ingredientsList);
+    } else {
+      let notFound = document.createElement("p");
+      notFound.innerText = "Recipe not found";
+      recipeContainer.append(notFound);
+    }
+  });
+  
+  
 }
